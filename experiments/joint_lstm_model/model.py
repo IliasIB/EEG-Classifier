a765832f711025f6ad74359e262f0776bebd467b
Ilias Bachiri <ilias.bachiri@student.kuleuven.be>
2019-11-06 18:02:45 +0100

    Cleaned up joint model

--- experiments/joint_lstm_model/model.py
+++ experiments/joint_lstm_model/model.py
@@ -15,17 +15,15 @@ def joint_lstm_model(filters=1, kernel_size=16, time_window=640):
     env2 = tf.keras.layers.Input(shape=(time_window, 1), name="input_env2")
 
     encoder = model.get_layer("max_pooling1d_1").output
-    # lstm_flat = tf.keras.layers.Flatten()(encoder)
-    lstm_pre = tf.keras.layers.Dense(1, activation="sigmoid")(encoder)
-    # lstm = tf.keras.layers.LSTM(16, input_shape=(25, 16))(encoder)
+    enc_dense = tf.keras.layers.Dense(1, activation="sigmoid")(encoder)
 
     flat1 = tf.keras.layers.Flatten()(env1)
     flat2 = tf.keras.layers.Flatten()(env2)
     env1_dense = tf.keras.layers.Dense(160, activation="sigmoid")(flat1)
     env2_dense = tf.keras.layers.Dense(160, activation="sigmoid")(flat2)
 
-    dot1 = tf.keras.layers.Dot(0, normalize=True)([lstm_pre, env1_dense])
-    dot2 = tf.keras.layers.Dot(0, normalize=True)([lstm_pre, env2_dense])
+    dot1 = tf.keras.layers.Dot(0, normalize=True)([enc_dense, env1_dense])
+    dot2 = tf.keras.layers.Dot(0, normalize=True)([enc_dense, env2_dense])
 
     concat = tf.keras.layers.Concatenate()([dot1, dot2])
     flat = tf.keras.layers.Flatten()(concat)
