5d7a3c6c1b0b965b78ff764082b032f05ea17003
Ilias Bachiri <ilias.bachiri@student.kuleuven.be>
2019-12-04 14:58:07 +0100

    Added visualization

--- experiments/sequence_lstm_model/evaluation.py
+++ experiments/sequence_lstm_model/evaluation.py
@@ -23,7 +23,7 @@ def evaluate():
 
     ds_creator = TFRecordsDatasetBuilder(folder=data_folder)
     test_datasets = ds_creator.prepare("test", batch_equalizer=Default2EnvBatchEqualizer(),
-                                       batch_size=64, window=320)
+                                       batch_size=64, window=128)
 
     model = tf.keras.models.load_model(os.path.join(cwd, "output", "best_model.h5"))
 
