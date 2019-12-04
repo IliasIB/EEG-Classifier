593593ff70d8486eff40695ffb2416e8ed631410
Ilias Bachiri <ilias.bachiri@student.kuleuven.be>
2019-12-04 11:55:23 +0100

    Lowered amount of parameters

--- experiments/sequence_lstm_model/model.py
+++ experiments/sequence_lstm_model/model.py
@@ -7,14 +7,14 @@ def simple_lstm_model(filters=1, kernel_size=16, time_window=640):
     env2 = tf.keras.layers.Input(shape=(time_window, 1))
 
     conv1d = tf.keras.layers.Conv1D(6, strides=1, kernel_size=1)(eeg)
-    conv1d2 = tf.keras.layers.Conv1D(16, strides=2, kernel_size=kernel_size, activation="relu")(conv1d)
-    conv1d_layer = tf.keras.layers.Conv1D(48, strides=2, kernel_size=kernel_size, activation="relu")
+    conv1d2 = tf.keras.layers.Conv1D(24, strides=2, kernel_size=kernel_size, activation="relu")(conv1d)
+    conv1d_layer = tf.keras.layers.Conv1D(16, strides=2, kernel_size=kernel_size, activation="relu")
     conv1d_env1 = conv1d_layer(env1)
     conv1d_env2 = conv1d_layer(env2)
 
     # lstm1 = tf.keras.layers.Bidirectional(tf.keras.layers.CuDNNLSTM(8, return_sequences=True))(conv1d2)
-    lstm3 = tf.keras.layers.CuDNNLSTM(48, return_sequences=True)(conv1d2)
-    lstm_layer = tf.keras.layers.CuDNNLSTM(48, return_sequences=True)
+    lstm3 = tf.keras.layers.CuDNNLSTM(32, return_sequences=True)(conv1d2)
+    lstm_layer = tf.keras.layers.CuDNNLSTM(32, return_sequences=True)
     lstm_env1 = lstm_layer(conv1d_env1)
     lstm_env2 = lstm_layer(conv1d_env2)
 
